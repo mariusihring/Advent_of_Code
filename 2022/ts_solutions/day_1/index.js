9ff992ef981aa953ff26b5d9300a03bd1296ecb6
Marius <marius.ihring@icloud.com>
2022-12-04 14:42:53 +0100

    refactored day 1 ts

--- 2022/ts_solutions/day_1/index.js
+++ 2022/ts_solutions/day_1/index.js
@@ -2,19 +2,6 @@ import fs from "fs"
 
 let input = fs.readFileSync("./input.txt", "utf-8").split("\n\n")
 let max = 0
-input.map((element) => {
- let calories = element.split("\n")
-  let sum = 0
-  calories.forEach((item) => {
-    let myNumber = parseInt(item)
-    sum += myNumber
-  })
-  if (sum > max) {
-    max = sum
-  }
-})
-
-console.log(max)
 let myArray = []
 input.map((element) => {
  let calories = element.split("\n")
@@ -23,7 +10,10 @@ input.map((element) => {
     let myNumber = parseInt(item)
     sum += myNumber
   })
-  myArray.push(sum) 
+  myArray.push(sum)
+  if (sum > max) {
+    max = sum
+  }
 })
 myArray = myArray.sort(function (a,b) {return a - b})
 myArray.pop()
