55c0ec31b32c9ad0531913386014c4e366c8db7e
Marius <marius.ihring@icloud.com>
2022-12-04 14:39:30 +0100

    day 1 part 2

--- 2022/ts_solutions/day_1/index.js
+++ 2022/ts_solutions/day_1/index.js
@@ -15,4 +15,19 @@ input.map((element) => {
 })
 
 console.log(max)
+let myArray = []
+input.map((element) => {
+ let calories = element.split("\n")
+  let sum = 0
+  calories.forEach((item) => {
+    let myNumber = parseInt(item)
+    sum += myNumber
+  })
+  myArray.push(sum) 
+})
+myArray = myArray.sort(function (a,b) {return a - b})
+myArray.pop()
+myArray = myArray.reverse()
+console.log(myArray[0] + myArray[1] + myArray[2])
+
 
