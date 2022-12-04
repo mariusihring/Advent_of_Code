b85f2339dd3184df2f1873c8aa714a0753c3d6fc
Marius <marius.ihring@icloud.com>
2022-12-04 20:08:13 +0100

    day 3 part 2

--- 2022/ts_solutions/day_3/index.js
+++ 2022/ts_solutions/day_3/index.js
@@ -3,6 +3,7 @@ import fs from 'fs'
 const test_input = fs.readFileSync("./test_input.txt", "utf-8").split("\n")
 const input = fs.readFileSync("./input.txt", "utf-8").split("\n")
 
+const second = fs.readFileSync("./input.txt", "utf-8")
 
 let score = 0
 for (const half of input) {
@@ -24,3 +25,19 @@ for (const half of input) {
 
 
 console.log(score)
+
+
+let sum = 0
+second.match(/(?:.+\n?){3}/g).map((team) => {
+  const [firstElv, secondElv, thirdElv] = team.split("\n")
+  const allItems = [firstElv.split(""), secondElv.split(""), thirdElv.split("")]
+  const [TheOneItem] = allItems.reduce((x, i) => x.filter(element => i.includes(element)))
+  if (TheOneItem === TheOneItem.toUpperCase()) {
+    sum += TheOneItem.charCodeAt() - 38
+  } else {
+
+    sum += TheOneItem.charCodeAt() - 96
+  }
+})
+
+console.log(sum)
